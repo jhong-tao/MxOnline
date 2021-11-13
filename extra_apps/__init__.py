171d94706c02b142e9743914fecd03174daa3de9
jhong.tao <jhong.tao@qq.com>
2021-11-13 19:15:10 +0800

    'init'

---
+++ extra_apps/__init__.py
@@ -0,0 +1,12 @@
+#!/usr/bin/env python
+# -*- coding: UTF-8 -*-
+"""
+==================================================
+@Project -> File   ：MxOnline -> __init__.py.py
+@IDE    ：PyCharm
+@Author ：jhong.tao
+@Date   ：2021/6/7 10:34
+@Desc   ：
+==================================================
+"""
+$END$
